d42db66bac7600af2275e1b6bacf22f9b5fcab6d
Sibindra <timalsinasibindra@gmail.com>
2024-01-09 06:34:05 +0545

    init:frontend

---
+++ frontend/app/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from '@/components/shadcn/ui/button'
+
+export default function Home() {
+  return (
+    <main>
+
+      <div className='flex flex-col gap-5 w-full justify-center items-center'>
+        <p className=' text-lg font-medium '> tailwind test</p>
+        <Button>shadcn test</Button>
+      </div>
+    </main>
+  )
+}
