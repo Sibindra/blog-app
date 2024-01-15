fe47cab3d0a60107c289cdefc3a181e06bc45077
Sibindra <timalsinasibindra@gmail.com>
2024-01-15 12:38:36 +0545

    feat: readtime and datetime

--- frontend/app/(main)/blog/page.tsx
+++ frontend/app/(main)/blog/page.tsx
@@ -1,14 +1,12 @@
 import BlogCard from "@/components/cards/blog-card"
 import MDXViewer from "@/components/markdown/viewer"
-import { Separator } from "@/components/shadcn/ui/separator"
 import { dummyBlogData } from "@/dummy-datas/blogs.data"
 import { cn } from "@/lib/utils"
-import { MDXRemote } from "next-mdx-remote/rsc"
 import Image from "next/image"
 
 export default async function BlogPage() {
-  const res = await fetch('https://raw.githubusercontent.com/Sibindra/testmds/main/test.md' , {
-    next:{
+  const res = await fetch('https://raw.githubusercontent.com/Sibindra/testmds/main/test.md', {
+    next: {
       revalidate: 10000
     }
   })
@@ -20,6 +18,11 @@ export default async function BlogPage() {
 
       <div className=" w-full md:w-3/4 px-10 flex flex-col gap-10">
         <h1 className="  font-bold text-4xl tracking-wide mt-5">Installing Next.js and PostgreSQL on macOS, Windows, and Linux</h1>
+
+        <div className=" font-medium text-sm">
+          10 min read • Mon 15 Jan 2023 • 12:30 PM
+        </div>
+
         <ImgContainer src={"https://images.unsplash.com/photo-1704928341414-5ae341023539?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw3NXx8fGVufDB8fHx8fA%3D%3D"} />
         <MDXViewer markdownData={markdown} />
       </div>
